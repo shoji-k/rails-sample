d801fedda1a6d7f28fb590b95f89332319b31826
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-07 17:26:03 +0900

    rails generate Micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   mount Ckeditor::Engine => '/ckeditor'
   resources :editors
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629095522) do
+ActiveRecord::Schema.define(version: 20170707081137) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170629095522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "tex"
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
