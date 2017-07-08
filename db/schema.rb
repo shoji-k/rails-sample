e63a9e86ac90d1a98ff8acd2bd28b24572d2ca4b
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-08 18:59:31 +0900

    add email index and before_save email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707081137) do
+ActiveRecord::Schema.define(version: 20170708094745) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170707081137) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
