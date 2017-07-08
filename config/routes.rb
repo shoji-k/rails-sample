b450c881066f6a345c0454804fc0048bb0ce278a
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-08 17:48:22 +0900

    change some urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   resources :microposts
   mount Ckeditor::Engine => '/ckeditor'
   resources :editors
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'editors#index'
+  # root 'editors#index'
 end
