b0050d3e9bea68fb76cf11ae847d3f378672cc3f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-09 23:32:35 +0900

    change sessions url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   get 'users/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
