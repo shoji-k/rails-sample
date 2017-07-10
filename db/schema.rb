448121f25c712720df0f051e0b490a4aafc278df
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-10 17:54:13 +0900

    prepare cookie to save user.id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708100053) do
+ActiveRecord::Schema.define(version: 20170710083609) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170708100053) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
