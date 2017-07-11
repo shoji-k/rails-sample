c92ffd0d03d5b3b46eea3472afbbe71c89f09e2f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-11 12:15:02 +0900

    add admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710083609) do
+ActiveRecord::Schema.define(version: 20170711031142) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -42,10 +42,11 @@ ActiveRecord::Schema.define(version: 20170710083609) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
