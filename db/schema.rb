c452a302434aaad3a976525686842a61675c3777
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-12 21:33:37 +0900

    add picture columns to micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712112138) do
+ActiveRecord::Schema.define(version: 20170712123313) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170712112138) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
