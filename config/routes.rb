752b02a75fe2e1ba82d17c5a3d77fcbba664f927
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-07-13 00:02:20 +0900

    show followers and followings

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'editors#index'
 end
