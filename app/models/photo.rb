94f2614a12994e477b881b57f24edc42f89a1c0f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-08-09 16:22:49 +0900

    prepare to upload files for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,11 @@
 class Photo < ApplicationRecord
+  mount_uploader :name, PhotoUploader
+  validate :name_size
+
+  private
+    def name_size
+      if name.size > 5.megabytes
+        errors.add(:name, "should be less than 5MB")
+      end
+    end
 end
